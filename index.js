923bd8c4384f7bd72416a32bbc2a89209c19e4b3
Konstantin Bobkov <mrglorf@gmail.com>
2023-04-04 08:57:53 +0400

    feat: add note tag

--- index.js
+++ index.js
@@ -26,13 +26,28 @@ class Note {
     this._deleteButton = this._element.querySelector(
       ".note__button_type_delete"
     );
+    this._addTagButton = this._element.querySelector(
+      ".note__button_type_add-tag"
+    );
+    this._tagInput = this._element.querySelector(".note__tag-input");
+    this._saveTagButton = this._element.querySelector(
+      ".note__button_type_save-tag"
+    );
+    this._resetTagButton = this._element.querySelector(
+      ".note__button_type_reset-tag"
+    );
+    this._tagButton = this._element.querySelector(".note__button_type_tag");
+    this._noteButtonList = this._element.querySelectorAll(".note__button");
     this._setEventListeners();
     return this._element;
   }
+
   _setEventListeners() {
+    this._preventDefaultButton();
     this._editButton.addEventListener("click", () => {
       console.log("Клик блять!");
       this._enableTextFields();
+      this._noteTitle.focus();
       this._disableButton(this._editButton);
       this._enableButton(this._saveButton);
       this._enableButton(this._resetButton);
@@ -44,8 +59,43 @@ class Note {
       this._enableButton(this._editButton);
     });
     this._resetButton.addEventListener("click", () => {
-      this._noteTitle.value = "";
-      this._noteText.value = "";
+      this._resetTextFields();
+    });
+    this._addTagButton.addEventListener("click", () => {
+      this._disableButton(this._addTagButton);
+      this._disableButton(this._deleteButton);
+      this._enableButton(this._saveTagButton);
+      this._enableButton(this._resetTagButton);
+      this._enableButton(this._tagInput);
+      this._tagInput.focus();
+    });
+    this._resetTagButton.addEventListener("click", () => {
+      this._enableButton(this._addTagButton);
+      this._enableButton(this._deleteButton);
+      this._disableButton(this._saveTagButton);
+      this._disableButton(this._resetTagButton);
+      this._disableButton(this._tagInput);
+      this._tagInput.value = "";
+    })
+    this._saveTagButton.addEventListener("click", () => {
+      this._disableButton(this._addTagButton);
+      this._enableButton(this._tagButton);
+      this._enableButton(this._deleteButton);
+      this._tagButton.textContent = this._tagInput.value;
+      this._disableButton(this._saveTagButton);
+      this._disableButton(this._resetTagButton);
+      this._disableButton(this._tagInput);
+      
+    })
+    this._deleteButton.addEventListener("click", () => {
+      this._element.remove();
+    });
+  }
+  _preventDefaultButton() {
+    this._noteButtonList.forEach((item) => {
+      item.addEventListener("click", (evt) => {
+        evt.preventDefault();
+      });
     });
   }
   _enableTextFields() {
@@ -62,18 +112,23 @@ class Note {
   _disableButton(button) {
     button.classList.add("note__button_disabled");
   }
-}
-
-function addNewCard(note, container) {
-  container.prepend(note);
+  _resetTextFields() {
+    this._noteTitle.value = "";
+    this._noteText.value = "";
+  }
 }
 
 const notesContainer = document.querySelector(".notes__container");
+const addNoteButton = document.querySelector(".notes__add-button");
 
-console.log(notesContainer);
+function addNewCard(container) {
+  const NoteElement = new Note("#NoteTemplate");
+  const newNote = NoteElement.generateNote();
+  container.prepend(newNote);
+}
 
-const note = new Note("#NoteTemplate");
-const newNote = note.generateNote();
-addNewCard(newNote, notesContainer);
-const notesOverlay = notesContainer.querySelectorAll(".note");
-console.log(notesOverlay);
+addNoteButton.addEventListener("click", () => {
+  addNewCard(notesContainer);
+});
+
+addNewCard(notesContainer);
